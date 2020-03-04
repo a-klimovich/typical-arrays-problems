04a428f19b849cec05c533fbe2eb8698aac814d0
Arthur <kom9toshka@gmail.com>
2020-03-04 11:57:03 +0300

    add: done

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min (array = 0) {
+  return (array != 0) 
+    ? array.sort((a,b)=>{return a - b}).shift()
+    : 0
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max (array = 0) {
+  return (array != 0) 
+    ? array.sort((a,b)=>{return a - b}).pop()
+    : 0
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg (array = 0) {
+  return (array != 0) 
+    ? array.reduce((acc, curr) => acc + curr) / array.length
+    : 0
 }
